b64f1f6b0f5f7fd89f6ea076f0976f8c4f7d7b99
Dexter Pai <dr.pyiephonepai@gmail.com>
2024-01-15 22:19:05 -0600

    solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let ans = '';
+    for (let i = text.length - 1; i >= 0; i--) {
+        ans += text[i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
