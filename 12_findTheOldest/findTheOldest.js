b07ccba353dee236aee58b46b1d483060dda11e5
Dexter Pai <dr.pyiephonepai@gmail.com>
2024-01-28 22:58:06 -0600

    solved 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+const currentYear = new Date().getFullYear();
+const years = peopleArray.map(object => {
+    if (object.yearOfDeath === undefined) return currentYear - object.yearOfBirth;
+    else return object.yearOfDeath - object.yearOfBirth;
+})
+const reduced = years.reduce((largest, current) => {
+    if (largest > current) return largest;
+    else return current;
+})
+const index = years.findIndex(e => e == reduced);
+return peopleArray[index];
 };
 
 // Do not edit below this line
