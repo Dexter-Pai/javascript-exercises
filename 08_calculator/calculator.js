aa58b0f02a011f5dd56975c75bd948f8a36a20ec
Dexter Pai <dr.pyiephonepai@gmail.com>
2024-01-28 18:33:10 -0600

    solved 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
 const sum = function() {
-	
+  return [...arguments[0]].reduce((total, value) => total += value , 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numarray) {
+  return numarray.reduce((total, value) => total *= value)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(numOfFactorial) {
+  let array = [];
+  if (numOfFactorial === 0) return 1;
+	for(let i = 1; i <= numOfFactorial; i++) array.push(i);
+  return array.reduce((total, value) => total *= value);
 };
 
 // Do not edit below this line
